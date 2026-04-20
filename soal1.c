cd6dc913d3c0a81f72db1c4750a923b5dcf0f64f
Crowsten <Jonathanchandra2109@gmail.com>
2026-04-20 10:14:50 +0700

    Fix formatting and include math.h for floor function

--- soal1.c
+++ soal1.c
@@ -61,14 +61,15 @@ int main() {
             }
 
             if(kiri && kanan){
-            arr[i] = floor((var_kiri + var_kanan)/2.0) ;
-            } else if (kiri) {
-                arr[i] = var_kiri;
-            } else if (kanan) {
-                arr[i] = var_kanan;
-            } else {
-                arr[i] = 0;
-            }
+                #include <math.h>
+                arr[i] = floor((var_kiri + var_kanan)/2.0) ;
+                } else if (kiri) {
+                    arr[i] = var_kiri;
+                } else if (kanan) {
+                    arr[i] = var_kanan;
+                } else {
+                    arr[i] = 0;
+                }
         }
     }
 
