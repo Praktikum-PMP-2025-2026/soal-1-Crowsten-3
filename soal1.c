df94278294d34bcba098ec43dfaaae9e02e46950
Crowsten <Jonathanchandra2109@gmail.com>
2026-04-20 09:10:24 +0700

    Fix formatting and memory allocation in soal1.c

--- soal1.c
+++ soal1.c
@@ -2,7 +2,7 @@
  *   Modul               : 2 – Data Manipulation and External Files
  *   Hari dan Tanggal    : Senin 20 April 2026
  *   Nama (NIM)          : Jonathan Chandra (13224103)
- *   Nama File           : soal1.c
+ *   Nama File           : Soal1.c
  *   Deskripsi           : Pemulihan Sensor Fragmen
  * 
  */
@@ -19,59 +19,59 @@ int nilai_max(int a, int b){
         return b;
     }
 }
-int main (){
+
+int main() {
     int N;
-    scanf("%d",&N);
     
-    int *arr = (int *)malloc(N * sizeof(int));
-    if(arr== NULL){
+    if (scanf("%d",&N)!=1) {
+        return 0;
+    }
+
+    int *arr = (int*)malloc(N*sizeof(int));
+    if (arr == NULL) {
+        printf("Gagal mengalokasikan memori.\n");
         return 1;
     }
 
-    for(int i=0;i<N;i++){
-        scanf("%d",&arr[i]);
+    for (int i=0; i<N;++i) {
+        scanf("%d", &arr[i]);
     }
 
-    for(int i=0;i<N;i++){
-        if(arr[i]==-1){
-            int kiri=0;
-            int var_kiri=0;
+    for (int i=0;i<N;++i) {
+        if (arr[i] == -1) {
+            int kiri = 0; 
+            int var_kiri = 0;
 
-            if(i>0){
-                kiri=1;
-                var_kiri=arr[i-1];
+            if (i > 0) {
+                kiri = 1;
+                var_kiri = arr[i - 1];
             }
 
-            int kanan=0;
-            int var_kanan =0;
+            int kanan = 0;
+            int var_kanan = 0;
 
-            for(int j=i+1;j<N;++j){
-            if(arr[j]!=-1){
-                int kanan=1;
-                int var_kanan= arr[j];
-                break;
+            for (int j=i+1;j<N;++j) {
+                if (arr[j] != -1) {
+                    kanan = 1;
+                    var_kanan = arr[j];
+                    break;
+                }
             }
-        }
-            if(kiri && kanan){
-            arr[i]= (int) floor((var_kiri + var_kanan)/2) ;
-            }
-            else if (kiri){
+
+            if (kiri && kanan) {
+                arr[i] = (int)floor((var_kiri + var_kanan) / 2.0);
+            } else if (kiri) {
                 arr[i] = var_kiri;
+            } else if (kanan) {
+                arr[i] = var_kanan;
+            } else {
+                arr[i] = 0;
             }
-            else if (kanan){
-                arr[i]= var_kanan;
-            }
-            else {
-                arr[i]=0;
-            }
-
         }
-
-        
     }
 
     printf("RECOVERED ");
-    for(int i=0;i<N;i++){
+    for(int i=0;i<N;++i){
         printf("%d ",arr[i]);
     }
     printf("\n");
