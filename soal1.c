056909d80445dd8cccd30f1ae9511d9b22b69d08
Crowsten <Jonathanchandra2109@gmail.com>
2026-04-20 10:12:28 +0700

    Fix formatting and include math.h in soal1.c

--- soal1.c
+++ soal1.c
@@ -20,6 +20,8 @@ int nilai_max(int a, int b){
     }
 }
 
+#include <math.h>
+
 int main() {
     int N;
     
@@ -59,7 +61,7 @@ int main() {
             }
 
             if(kiri && kanan){
-            arr[i]= floor((var_kiri + var_kanan)/2.0) ;
+            arr[i] = floor((var_kiri + var_kanan)/2.0) ;
             } else if (kiri) {
                 arr[i] = var_kiri;
             } else if (kanan) {
